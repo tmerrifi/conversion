17c7aeefc5d9afb634c1e637eacfb37275f75177
tmerrifi <tmerri4@uic.edu>
2013-04-25 17:11:02 -0500

    updated simple to only use commit

--- tests/simple_test/simple.c
+++ tests/simple_test/simple.c
@@ -9,7 +9,7 @@
 #define ARRAY_SIZE_BYTES (NUM_OF_PAGES * (1<<12))
 
 #define NUM_OF_RUNS 3
-#define NUM_OF_ITERATIONS 20
+#define NUM_OF_ITERATIONS 1000
 
 int sum_up(conv_seg * array_seg){
   int i,j, sum;
