44c712811d7bf64240bdb7cad8c9dfe57d3af306
tmerrifi <tmerri4@uic.edu>
2013-04-29 15:29:00 -0500

    logging

--- source/module/cv_commit.c
+++ source/module/cv_commit.c
@@ -255,8 +255,11 @@ void cv_commit_version_parallel(struct vm_area_struct * vma, unsigned long flags
 
   cv_stats_end(cv_seg, cv_user, 0, commit_latency);
 
-  /*printk(KSNAP_LOG_LEVEL "IN COMMIT %d, committed pages %d....our version num %lu committed %lu\n", 
-    current->pid, committed_pages, our_version_number, cv_seg->committed_version_num);*/
+
+#ifdef CONV_LOGGING_ON
+  printk(KSNAP_LOG_LEVEL "IN COMMIT %d, committed pages %d....our version num %lu committed %lu\n", 
+	 current->pid, committed_pages, our_version_number, cv_seg->committed_version_num);
+#endif
 
 }
 
