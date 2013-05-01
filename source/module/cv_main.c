83ba6a538bdbf96d66de4385774040062219b29f
tmerrifi <tmerri4@uic.edu>
2013-05-01 13:22:11 -0500

    logging

--- source/module/cv_main.c
+++ source/module/cv_main.c
@@ -55,7 +55,7 @@ void cv_msync(struct vm_area_struct * vma, unsigned long flags){
     cv_update_parallel(vma, flags);
   }
   getrawmonotonic(&ts2);
-  if (ts1.tv_nsec % 200 == 0){
+  if (ts1.tv_nsec % 200 == 0 && cv_stats_elapsed_time_ns(&ts1, &ts2) > 1000){
   printk(KSNAP_LOG_LEVEL "elapsed time %lu\n", cv_stats_elapsed_time_ns(&ts1, &ts2));
   }
 }
