f66231ca44d04f0992e8e4a49d81ee8d209f4452
tmerrifi <tmerri4@uic.edu>
2013-05-04 09:42:44 -0500

    adding files

--- source/module/cv_close.c
+++ source/module/cv_close.c
@@ -1,10 +1,16 @@
+#include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/mm.h>
 #include <linux/fs.h>
-#include <linux/string.h>
 #include <linux/path.h>
+#include <linux/string.h>
+#include <linux/rmap.h>
 #include <linux/highmem.h>
+#include <linux/pagemap.h>
 #include <linux/list.h>
+#include <asm/pgtable.h>
+#include <asm/tlbflush.h>
+
 
 #include "conversion.h"
 
